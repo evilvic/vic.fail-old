e80e7a1f1933a03e2574de5fbb5dcee98b2776f5
Víctor Peña Romero <vic.pero@icloud.com>
2023-07-24 11:42:04 -0600

    push generated

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,47 @@
+// tina/config.js
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: null,
+  // Get this from tina.io
+  token: null,
+  // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
